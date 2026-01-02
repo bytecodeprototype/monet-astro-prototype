b8db230f4bc51be673f79979f6b2c285e4f894df
Mike <mike2guz@gmail.com>
2026-01-02 11:43:40 -0500

    Fix redirect loop by simplifying astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,5 @@
 import { defineConfig } from "astro/config";
 
-const isProd = process.env.NODE_ENV === "production";
-const site =
-  process.env.SITE_URL ||
-  (isProd
-    ? "https://example.github.io/monet-v2-prototype"
-    : "http://localhost:4321");
-const base = isProd ? process.env.BASE_PATH || "/monet-v2-prototype" : "/";
-
 export default defineConfig({
-  site,
-  base,
+  site: "https://monetlabel.com",
 });
